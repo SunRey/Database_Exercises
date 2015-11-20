3c3fe083a2232fff5d6d3c41ecf6f9c2c426e919
Jonathan R <jreyes.satx@gmail.com>
2015-11-20 15:51:53 -0600

    finished update exercise with the ALTER / MODIFY COLUMN clauses within the update exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -10,5 +10,25 @@ SELECT name AS 'All albums by Michael Jackson'
 FROM albums
 WHERE artist = 'Michael Jackson';
 
+UPDATE albums
+SET sales = sales * 10;
+SELECT name as 'Title', sales AS 'New Sales' 
+FROM albums;
 
+ALTER TABLE albums
+MODIFY COLUMN release_date INT;
+
+UPDATE albums
+SET release_date = (release_date - 100)
+WHERE release_date < 1980;
+SELECT name AS 'Title', release_date AS 'Now Released'
+FROM albums
+WHERE release_date < 1980;
+
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT artist AS 'New Jackson'
+FROM albums
+WHERE artist = 'Peter Jackson';
 
