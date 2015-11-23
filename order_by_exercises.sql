e6a1c2a0ad0c5fb78c392e1132e871038ebf0a44
Jonathan R <jreyes.satx@gmail.com>
2015-11-23 14:45:15 -0600

    last part of the order by exercsies

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -20,10 +20,11 @@ WHERE last_name LIKE 'E%'
 AND last_name LIKE '%e'
 ORDER BY emp_no DESC;
 
-SELECT first_name, last_name, hire_date, emp_no
+SELECT first_name, last_name, hire_date, birth_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%12-25%' ;
+AND birth_date LIKE '%12-25%' 
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT first_name, last_name, birth_date
 FROM employees
