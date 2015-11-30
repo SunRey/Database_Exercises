21de2f675c9e3d1033c18407d6dce0858df7c793
Jonathan R <jreyes.satx@gmail.com>
2015-11-30 15:22:13 -0600

    join exercise: cleanned up with class walk through. Changed search for department name instead of department number. additional exercise part 2

--- join_exercises.sql
+++ join_exercises.sql
@@ -33,8 +33,8 @@ WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, dm.to_date
 FROM employees AS e
 JOIN dept_manager AS dm ON dm.emp_no = e.emp_no
-JOIN departments AS d ON d.dept_no = dm.
-WHERE dm.to_date > now();
+JOIN departments AS d ON d.dept_no = dm.dept_no
+WHERE dm.to_date > NOW();
 
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Manager Name'
 FROM employees AS e
@@ -46,7 +46,7 @@ SELECT t.title AS Title, count(de.dept_no) AS Count, de.to_date
 FROM dept_emp AS de
 JOIN titles AS t ON t.emp_no = de.emp_no
 JOIN departments AS d ON d.dept_no = de.dept_no
-WHERE d.dept_no = 'd009' AND t.to_date > now()
+WHERE d.dept_name = 'Customer Service' AND t.to_date > now()
 GROUP BY t.title;
 
 SELECT d.dept_name AS 'Departmant Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary AS Salary
