480c9b16950c3bb7eb441806d0e8cb7b3adf3b85
Jonathan R <jreyes.satx@gmail.com>
2015-11-30 12:58:56 -0600

    join exercises, done with web exercises. TODO additional exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -20,3 +20,19 @@ SELECT r.name, count(u.role_id)
 FROM users AS u
 RIGHT JOIN roles AS r ON r.id = u.role_id
 GROUP BY r.name;
+
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+JOIN dept_emp as de
+  ON de.emp_no = e.emp_no
+JOIN departments as d
+  ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name, dm.to_date
+FROM employees AS e
+JOIN dept_manager AS dm ON dm.emp_no = e.emp_no
+JOIN departments AS d ON d.dept_no = dm.dept_no
+WHERE dm.to_date > now();
+
